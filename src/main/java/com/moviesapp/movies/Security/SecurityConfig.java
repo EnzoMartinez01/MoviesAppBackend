2125eae57143635760e49c75266460cf56624795
Enzo Martinez <enzomartinez2412@gmail.com>
2025-08-13 21:12:18 -0500

    CORS

--- src/main/java/com/moviesapp/movies/Security/SecurityConfig.java
+++ src/main/java/com/moviesapp/movies/Security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList(
                 "http://localhost:4200",
-                "https://movies-app-test-em.vercel.app/"
+                "https://movies-app-test-em.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Content-Disposition"));
